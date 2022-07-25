622f9c233048ddaf153c5d0c096fc3c3d1548e3f
Fredy Bintang <you@example.com>
2022-07-25 16:28:47 +0700

    Middleware

--- routes/web.php
+++ routes/web.php
@@ -23,34 +23,38 @@ Route::get('/', function () {
     return view('main');
 })->name('home'); */
 
+//Middleware
+Route::middleware(['auth'])->group(function () {
+    Route::get('transaksi', [TransaksiController::class, 'index'])->name('index');
+    Route::get('member', [MembershipController::class, 'member'])->name('member');
+    Route::get('petugas', [PetugasController::class, 'petugas'])->name('petugas');
 
+    Route::get('home', [TransaksiController::class, 'home'])->name('home');
 
-Route::get('home', [TransaksiController::class, 'home'])->name('home');
-
-/*
+    /*
 Route::resource('transaksi', TransaksiControllers::class); */
 
 
-//Transaksi
-Route::post('store', [TransaksiController::class, 'store'])->name('store');
-Route::delete('delete/{transaksi:id}', [TransaksiController::class, 'delete'])->name('delete');
-Route::get('edit/{transaksi:id}', [TransaksiController::class, 'edit'])->name('edit');
-Route::put('update/{transaksi:id}', [TransaksiController::class, 'update'])->name('update');
+    //Transaksi
+    Route::post('store', [TransaksiController::class, 'store'])->name('store');
+    Route::delete('delete/{transaksi:id}', [TransaksiController::class, 'delete'])->name('delete');
+    Route::get('edit/{transaksi:id}', [TransaksiController::class, 'edit'])->name('edit');
+    Route::put('update/{transaksi:id}', [TransaksiController::class, 'update'])->name('update');
 
-//Member
-Route::get('/member.create', [MembershipController::class, 'tambah']);
-Route::post('add', [MembershipController::class, 'add'])->name('add');
-Route::delete('hapus/{member:id}', [MembershipController::class, 'hapus'])->name('hapus');
-Route::get('ubah/{member:id}', [MembershipController::class, 'ubah'])->name('ubah');
-Route::put('updated/{member:id}', [MembershipController::class, 'updated'])->name('updated');
+    //Member
+    Route::get('/member.create', [MembershipController::class, 'tambah']);
+    Route::post('add', [MembershipController::class, 'add'])->name('add');
+    Route::delete('hapus/{member:id}', [MembershipController::class, 'hapus'])->name('hapus');
+    Route::get('ubah/{member:id}', [MembershipController::class, 'ubah'])->name('ubah');
+    Route::put('updated/{member:id}', [MembershipController::class, 'updated'])->name('updated');
 
-//Petugas
-Route::get('/petugas.create', [PetugasController::class, 'baru']);
-Route::post('storea', [PetugasController::class, 'storea'])->name('storea');
-Route::delete('apus/{petugas:id}', [PetugasController::class, 'apus'])->name('apus');
-Route::get('edits/{petugas:id}', [PetugasController::class, 'edits'])->name('edits');
-Route::put('updates/{petugas:id}', [PetugasController::class, 'updates'])->name('updates');
-
+    //Petugas
+    Route::get('/petugas.create', [PetugasController::class, 'baru']);
+    Route::post('storea', [PetugasController::class, 'storea'])->name('storea');
+    Route::delete('apus/{petugas:id}', [PetugasController::class, 'apus'])->name('apus');
+    Route::get('edits/{petugas:id}', [PetugasController::class, 'edits'])->name('edits');
+    Route::put('updates/{petugas:id}', [PetugasController::class, 'updates'])->name('updates');
+});
 
 //Login
 Route::get('/login', [LoginController::class, 'view'])->name('login');
@@ -68,14 +72,9 @@ Route::get('/register', [LoginController::class, 'register'])->name('register');
 Route::post('/registeruser', [LoginController::class, 'registeruser'])->name('registeruser');
 
 
-//Login Multilevel
-/* Route::middleware(['auth', 'Ceklevel:admin'])->group(function () {
- */ Route::get('transaksi', [TransaksiController::class, 'index'])->name('index');
-    Route::get('member', [MembershipController::class, 'member'])->name('member');
-    Route::get('petugas', [PetugasController::class, 'petugas'])->name('petugas');
-/* });
-Route::middleware(['auth', 'Ceklevel:user'])->group(function () {
-}); */
+
+
+
 
 
 // Route::get('/member', function () {
